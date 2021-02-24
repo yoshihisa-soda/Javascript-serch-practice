cff57859731e204eb6aff61fb636f4163ef0f659
yoshihisa-soda <soda.06141031@gmail.com>
2021-02-24 22:43:43 +0900

    How to use properties and objects

--- prc3.js
+++ prc3.js
@@ -41,3 +41,78 @@ let bag = {
 // 角括弧はドット表記よりもはるかに強力
 // それらは任意のプロパティ名や変数を許容
 // 書くのはドットより面倒
+
+// プロパティの短縮系
+function makeUser(name, age) {
+    return {
+        name: name,
+        age: age,
+    };
+}
+
+let user = makeUser("John", 28);
+alert(user.name);
+
+// ↓↓↓
+function makeUser {
+    return {
+        name,
+        age,
+    }
+}
+
+let user = {
+    name: "soda",
+    age: 30,
+};
+
+// プロパティ名の制限（これは予約語だからダメ）
+let obj = {
+    for: 1,
+    let: 2,
+    return: 3,
+}
+
+alert( obj.for + obj.let + obj.return );  // 6
+
+let obj = {};
+obj.__proto__ = 5;
+alert(obj.__proto__);
+
+// プロパティの存在チェック
+let user = {};
+alert( user.noSuchProperty === undefined );
+
+// 構文: "key" in object
+let user = {name: "ikeike", age: 90};
+
+alert( "age" in user ); // true, user.age は存在する
+alert( "blabla" in user ); // false, user.blabla は存在しない
+
+let user = { age: 30 };
+
+let key = "age";
+alert( key in user ); // true, キーから名前を取り、そのプロパティをチェック
+
+let obj = {
+    test: undefined
+};
+  
+alert( obj.test ); // これは undefined, なので - このようなプロパティはなし?
+alert( "test" in obj ); // true, プロパティは存在します!
+
+// for ... in ループ
+for (key in object) {
+    // オブジェクトプロパティの各キーに対して本体を実行
+}
+
+let user = {
+    name: "John",
+    age: 30,
+    isAdmin: true
+};
+
+for (let key in user) {
+    alert(key); // // name, age, isAdmin（keys）
+    alert(user[key]); // ohn, 30, true（values）
+}
