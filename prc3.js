24fe9fc5d9dd54d047cf891e30d96cf99b2b4304
yoshihisa-soda <soda.06141031@gmail.com>
2021-02-25 23:17:33 +0900

    about object and task

--- prc3.js
+++ prc3.js
@@ -116,3 +116,92 @@ for (let key in user) {
     alert(key); // // name, age, isAdmin（keys）
     alert(user[key]); // ohn, 30, true（values）
 }
+
+
+// オブジェクトの順序付け
+let codes = {
+    "49": "Germany",
+    "41": "Switzerland",
+    "44": "Great Britain",
+    "1": "USA"
+}
+
+for (let code in codes) {
+    alert(code);
+}
+
+let user = {
+    name: "John",
+    surname: "Smith"
+};
+user.age = 25; // 1つ追加
+  
+// 非整数プロパティは作成順にリストされます
+for (let prop in user) {
+    alert( prop ); // name, surname, age
+}
+
+// オブジェクトはいくつかの特別な機能を持つ連想配列
+
+// タスク1
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+// タスク2
+function isEmpty(obj) {
+    for (let key in obj) {
+        return true;
+    }
+    return false;
+}
+
+// タスク3
+const user = {
+    name: "John"
+};
+// works
+user.name = "Pete";
+// error
+user = 123;
+
+// タスク4
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+let sum = 0;
+
+for (let salary in salaries) {
+    sum += salaries[salary];
+}
+
+alert(sum); // 390
+
+// タスク5
+
+// 呼び出し前
+let menu = {
+   width: 200,
+   height: 300,
+   title: "My menu"
+};
+  
+multiplyNumeric(menu);
+  
+// 呼び出し後
+menu = {
+   width: 400,
+   height: 600,
+   title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == 'number')
+            return obj[key] *= 2; 
+    }
+}
