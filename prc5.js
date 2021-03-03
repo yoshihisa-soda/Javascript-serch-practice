8cc4928855a5a41c451068f50acd80ae01639843
yoshihisa-soda <soda.06141031@gmail.com>
2021-03-03 21:44:22 +0900

    json parse and tojson

--- prc5.js
+++ prc5.js
@@ -89,3 +89,85 @@ let meetup = {
 meetup.place = room;        // meetup は room を参照
 room.occupiedBy = meetup;   // room は meetup を参照
 JSON.stringify(meetup);     // Error: Converting circular structure to JSON
+
+
+// 書式設定 spacer
+let user = {
+    name: "John",
+    age: 25,
+    roles: {
+      isAdmin: false,
+      isEditor: true
+    }
+};
+
+alert(JSON.stringify(user, null, 2));
+/* 
+JSON.stringify(user, null, 4) の場合
+{
+    "name": "John",
+    "age": 25,
+    "roles": {
+        "isAdmin": false,
+        "isEditor": true
+    }
+}
+*/
+
+// カスタムの "toJSON"
+let room = {
+    number: 23
+};
+
+let meetup = {
+    title: "Conference",
+    date: new Date(Date.UTC(2017, 0, 1)),
+    room
+};
+
+alert( JSON.stringify(room) ); // 23
+
+alert( JSON.stringify(meetup) );
+/*
+{
+    "title":"Conference",
+    "room": 23
+}
+*/
+
+
+// JSON.parse
+// 文字列化された配列
+let numbers = "[0, 1, 2, 3]";
+numbers = JSON.parse(numbers);
+alert( numbers[1] ); // 1
+
+// ネストされたオブジェクト
+let user = '{ "name": "John", "age": 35, "isAdmin": false, "friends": [0,1,2,3] }';
+user = JSON.parse(user);
+alert( user.friends[1] ); // 1
+
+// リバイバー(reviver)の利用
+// title: (meetup title), date: (meetup date)
+let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+let meetup = JSON.parse(str, function(key, value) {
+    if (key == 'date') return new Date(value);
+    return value;
+});
+
+alert( meetup.date.getDate() ); // 動作！
+
+// ネストされたものでもOK
+let schedule = `{
+    "meetups": [
+      {"title":"Conference","date":"2017-11-30T12:00:00.000Z"},
+      {"title":"Birthday","date":"2017-04-18T12:00:00.000Z"}
+    ]
+}`;
+  
+schedule = JSON.parse(schedule, function(key, value) {
+  if (key == 'date') return new Date(value);
+  return value;
+});
+
+alert( schedule.meetups[1].date.getDate() ); // これも動作します!
